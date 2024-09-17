d085c5dc981a31e34d88be3b1f3d4fea25913678
davidnjoroge1 <ndegwa.david@outlook.com>
2024-09-17 11:47:58 +0300

    first commit

--- ascend/ascend/settings.py
+++ ascend/ascend/settings.py
@@ -131,9 +131,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ascend', 'static'),
 ]
 #
-#if not DEBUG:
-#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
