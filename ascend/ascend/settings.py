abb744b56a99802cce6e25c3db44ac5a725a577f
davidnjoroge1 <ndegwa.david@outlook.com>
2024-11-18 22:38:54 +0300

    updated ascend

--- ascend/ascend/settings.py
+++ ascend/ascend/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'ascend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,6 +107,7 @@ DATABASES = {
         conn_max_age=600
     )
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
